ac73cd6353a2a4a11278f8ba8db31c2ac6129b0e
雪グリコシド結晶 <IGCyukira@outlook.com>
2025-11-12 17:15:36 +0800

    Remove horizontal overflow from main section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <SmoothScrollEnhancer />
-      <main className="relative snap-y snap-mandatory h-[100svh] overflow-y-auto overflow-x-hidden">
+      <main className="relative snap-y snap-mandatory h-[100svh] overflow-y-auto">
         <LockdownOverlay />
         <HeroSection />
         <TerminalSection id="terminal" />
