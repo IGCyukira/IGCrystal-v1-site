41764dfafea34fb687bb356aef146141e2a0f5fa
IGCrystal <IGCrystal@outlook.jp>
2026-02-13 11:21:52 +0800

    feat: add redirects in next.config.ts for improved navigation

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://revaea.com/igcrystal",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
